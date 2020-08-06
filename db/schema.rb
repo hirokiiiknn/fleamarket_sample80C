f160ece59b02e4a61dbbc61faceab18704b7cea5
Oki-yu <radsolars@yahoo.co.jp>
2020-08-06 17:41:15 +0900

    add

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 ActiveRecord::Schema.define(version: 2020_08_06_025116) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.integer "birthday", null: false
+    t.text "introduction"
+    t.string "avator"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
