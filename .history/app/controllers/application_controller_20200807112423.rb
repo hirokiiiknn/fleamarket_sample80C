096b4fced20200b3edddd6feed989528201dab91
jinkichi10 <67736186+jinkichi10@users.noreply.github.com>
2020-08-07 11:26:45 +0900

    0807_1126

--- .history/app/controllers/application_controller_20200807112423.rb
+++ .history/app/controllers/application_controller_20200807112423.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday, :introduction, :avator])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :family_name_kana, :family_name_kana])
   end
 
 
