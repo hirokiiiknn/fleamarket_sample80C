82a08957674cae7028d489f04a2672b24b7bfbdc
Oki-yu <radsolars@yahoo.co.jp>
2020-08-07 10:49:40 +0900

    add comentout

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_08_06_101019) do
 
-ActiveRecord::Schema.define(version: 0) do
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
-
-ActiveRecord::Schema.define(version: 2020_08_06_060528) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
@@ -25,8 +26,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_060528) do
     t.integer "days", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
-ActiveRecord::Schema.define(version: 2020_08_06_025116) do
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "nickname", null: false
@@ -46,8 +46,6 @@ ActiveRecord::Schema.define(version: 2020_08_06_025116) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
-
 
 end
