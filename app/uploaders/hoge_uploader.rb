77dc44221159d348fab13f547d8c23822f26720f
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-08-09 15:44:14 +0900

    0809

--- app/uploaders/hoge_uploader.rb
+++ app/uploaders/hoge_uploader.rb
@@ -1,12 +1,19 @@
 class HogeUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
+  include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  process :resize_to_fit => [800, 800]
 
+  # 保存形式をJPGにする
+  process :convert => 'jpg'
+
+  # サムネイルを生成する設定
+  version :thumb do
+    process :resize_to_fit => [300, 300]
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -35,9 +42,9 @@ class HogeUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
