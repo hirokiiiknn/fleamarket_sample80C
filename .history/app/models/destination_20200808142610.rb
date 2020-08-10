cfc0f724ca10bff927632b18e65805003216adef
jinkichi10 <67736186+jinkichi10@users.noreply.github.com>
2020-08-10 16:51:43 +0900

    1651

--- .history/app/models/destination_20200808142610.rb
+++
@@ -1,7 +0,0 @@
-class Destination < ApplicationRecord
-  belongs_to :user, optional: true
-
-  validates :first_name_destination, :family_name_destination, :first_name_destination_kana, 
-  ,presence: true
-
-end
