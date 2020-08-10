0f476826f712887108e1cd1045e7c5d299dcf04c
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-08-10 12:08:08 +0900

    0810

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,11 @@ class User < ApplicationRecord
   validates :birthday, presence: true
 
   has_one :destination
-  
+
+  #userが「買った」商品 →buyed_items
+  has_many :bought_items, foreign_key: "buyer_id", class_name: "Item" 
+  #userが 「現在売っている」商品 →selling_items
+  has_many :selling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
+  #userが「既に売った」商品 →sold_items
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
 end
