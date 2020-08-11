d224522b4d945a74b1e307f1943f6a3d0b42b08e
alpha-i5 <ywgyg497@gmail.com>
2020-08-11 19:16:27 +0900

    top-end

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_08_10_080230) do
 
-
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "customer_id", null: false
@@ -21,9 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_080230) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
-
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
@@ -31,10 +28,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_080230) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
-
-
-  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
-
+  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name_destination", null: false
     t.string "family_name_destination", null: false
     t.string "first_name_destination_kana", null: false
@@ -56,10 +50,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_080230) do
     t.index ["item_id"], name: "index_images_on_item_id"
   end
 
-
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
-
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
@@ -93,7 +84,5 @@ ActiveRecord::Schema.define(version: 2020_08_10_080230) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "destinations", "users"
   add_foreign_key "images", "items"
 end
