313e9f7ffc36e278d79f232d1421bc87a1ed9fda
jinkichi10 <67736186+jinkichi10@users.noreply.github.com>
2020-08-12 16:40:20 +0900

    1640

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,6 @@ class ItemsController < ApplicationController
 
 
 
-
   def index
     @items = Item.joins(:images).select('items.*, images.image').order('created_at DESC')
   end
@@ -50,7 +49,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-  
+    
   end
 
   def search
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_132425) do
+ActiveRecord::Schema.define(version: 2020_08_12_054141) do
 
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_132425) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_132425) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
-  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name_destination", null: false
     t.string "family_name_destination", null: false
     t.string "first_name_destination_kana", null: false
@@ -51,15 +51,14 @@ ActiveRecord::Schema.define(version: 2020_08_10_132425) do
     t.index ["user_id"], name: "index_destinations_on_user_id"
   end
 
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
-    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_images_on_item_id"
+    t.integer "item_id"
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
@@ -78,7 +77,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_132425) do
     t.index ["image_id"], name: "index_items_on_image_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "first_name", null: false
     t.string "family_name", null: false
@@ -98,7 +97,5 @@ ActiveRecord::Schema.define(version: 2020_08_10_132425) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "destinations", "users"
-  add_foreign_key "images", "items"
 end
