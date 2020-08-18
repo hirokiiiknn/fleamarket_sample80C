0ce6bc43989b971af8108b45baf133cc96f17bd0
jinkichi10 <67736186+jinkichi10@users.noreply.github.com>
2020-08-18 11:43:30 +0900

    1143

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,6 +1,6 @@
-$(document).on('turbolinks:load', ()=> {
+$(document() {
   // 画像用のinputを生成する関数
-  const buildFileField = (num)=> {
+  const buildFileField = (num) => {
     const html = `<div data-index="${num}" class="js-file_group">
                     <input class="js-file" type="file"
                     name="item[images_attributes][${num}][image]"
@@ -10,26 +10,26 @@ $(document).on('turbolinks:load', ()=> {
     return html;
   }
 
-  const buildImg = (index, url)=> {
+  const buildImg = (index, url) => {
     const html = `<img data-index="${index}" src=${url} width="100px" height="100px">`;
     return html;
   }
 
-  
+
 
   // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  let fileIndex =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   // 既に使われているindexを除外
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
   $('.hidden-destroy').hide();
 
-  $('#image-box').on('change', '.js-file', function(e) {
+  $('#image-box').on('change', '.js-file', function (e) {
     const targetIndex = $(this).parent().data('index');
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
-   
+
     // 該当indexを持つimgタグがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('image', blobUrl);
@@ -43,7 +43,7 @@ $(document).on('turbolinks:load', ()=> {
     }
   });
 
-  $('#image-box').on('click', '.js-remove', function() {
+  $('#image-box').on('click', '.js-remove', function () {
     const targetIndex = $(this).parent().data('index')
     // 該当indexを振られているチェックボックスを取得する
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
@@ -53,7 +53,7 @@ $(document).on('turbolinks:load', ()=> {
     $(`img[data-index="${targetIndex}"]`).remove();
     // 画像入力欄が0個にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-      
+
   });
-  
+
 });
