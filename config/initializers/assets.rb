ae35fd77dc3720779845dcd988b8c6724ad3f9d6
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-08-20 17:24:28 +0900

    Update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.precompile += %w(
   admin.js admin.css modules/items modules/sign_up modules/new_items modules/show modules/post_done modules/card_main_buy)
 
 Rails.application.config.assets.precompile += %w( modules/sessions-new.css modules/delete_done.css)
+Rails.application.config.assets.precompile += %w( modules/delete_done.css )
