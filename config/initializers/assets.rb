91373ab6f01bbe9c4f1eb8839e39ae2143f286e8
mee-022 <jycfp268@yahoo.co.jp>
2020-08-21 04:38:26 +0900

    add users haml

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( 
-  admin.js admin.css modules/items modules/sign_up modules/new_items modules/show modules/post_done modules/card_main_buy)
+  admin.js admin.css modules/items modules/sign_up modules/new_items modules/show modules/post_done modules/card_main_buy modules/users_index)
 
 Rails.application.config.assets.precompile += %w( modules/sessions-new.css modules/delete_done.css)
 Rails.application.config.assets.precompile += %w( modules/delete_done.css )
