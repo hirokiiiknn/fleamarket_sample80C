ab68027bbfaf722828c1d9a9d2095dd5d834cab0
jinkichi10 <67736186+jinkichi10@users.noreply.github.com>
2020-08-20 21:48:21 +0900

    マージlast

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,4 +15,4 @@ Rails.application.config.assets.precompile += %w(
   admin.js admin.css modules/items modules/sign_up modules/new_items modules/show modules/post_done modules/card_main_buy)
 
 Rails.application.config.assets.precompile += %w( modules/sessions-new.css modules/delete_done.css)
-Rails.application.config.assets.precompile += %w( modules/delete_done.css )
+Rails.application.config.assets.precompile += %w( modules/delete_done.css modules/new_card.css modules/_done.css)
